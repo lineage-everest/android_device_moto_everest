bd21f6af4a92600720ad4671f0de80029140c565
Schischu <schischu65@gmail.com>
2014-01-04 12:59:18 +0100

    Add cm-11.0 support

--- cm.mk
+++ cm.mk
@@ -14,19 +14,16 @@
 # limitations under the License.
 #
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
-
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/moto/everest/device.mk)
 
-PRODUCT_NAME := omni_everest
+PRODUCT_NAME := cm_everest
 PRODUCT_DEVICE := everest
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Xoom 3G
